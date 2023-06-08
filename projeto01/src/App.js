8510637670dba78ba5b965aa60ebc6b98e248679
Carloseduardomelo <edu464377@gmail.com>
2023-06-08 17:48:48 -0300

    01

---
+++ projeto01/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+
+  let none = 'carlos'
+  
+  alert(none)
+
+  return (
+    document.
+    <body className="App">
+      <h1> Ola mundo </h1>
+      <p> Meu primeiro projeto em react </p>
+    </body>
+  );
+}
+
+export default App;
