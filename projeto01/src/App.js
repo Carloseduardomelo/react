1aa7798ee7f99e7347ecab65403a96d6e0c407c6
Carloseduardomelo <edu464377@gmail.com>
2023-06-08 21:30:15 -0300

    .

--- projeto01/src/App.js
+++ projeto01/src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
 
 function App() {
-
-  let none = 'carlos'
   
-  alert(none)
 
+  
   return (
-    document.
     <body className="App">
       <h1> Ola mundo </h1>
       <p> Meu primeiro projeto em react </p>
