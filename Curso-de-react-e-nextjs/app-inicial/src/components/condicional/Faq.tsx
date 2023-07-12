3ac92263439d566f55bc8ca03d4dd1bf97a664f7
Carloseduardomelo <127193594+Carloseduardomelo@users.noreply.github.com>
2023-07-12 16:06:09 -0300

    att

--- Curso-de-react-e-nextjs/app-inicial/src/components/condicional/Faq.tsx
+++ Curso-de-react-e-nextjs/app-inicial/src/components/condicional/Faq.tsx
@@ -1,7 +1,13 @@
+import Pergunta from "./Per";
+
 export default function Faq(){
     return(
-        <div className={`flex items-center justify-center m-10`}>
-            FAq
+        <div className={`flex flex-col items-center justify-center m-12 gap-10 text-center`}>
+
+            <Pergunta texte="O que é que quanto mais se tira, mais ele cresce?" resposta="Um buraco"/>
+            <Pergunta  texte="O que é que tem quatro patas, mas não consegue andar?" resposta="Uma messa"/>
+            <Pergunta texte="O que é que todo mundo tem, mas ninguém pode perder?" resposta="Um sobrenome"/>
+            
         </div>
     )
 }
--- Curso-de-react-e-nextjs/app-inicial/src/components/condicional/Per.tsx
+++ Curso-de-react-e-nextjs/app-inicial/src/components/condicional/Per.tsx
@@ -1,7 +1,26 @@
-export default function Pergunta(){
+import {useState} from "react"
+
+interface PerguntaProps{
+    texte: string;
+    resposta: string;
+}
+
+export default function Pergunta(props: PerguntaProps){
+
+    const [Aberta, setAberta] = useState<Boolean>(false)
+
     return(
-        <div>
-            pergunta
+        <div className={`border border-zinc-100 p-[0px] rounded-[5px] overflow-hidden`}>
+            <div className={`bg-zinc-700 p-5 cursor-pointer select-none`} onClick={() => setAberta(!Aberta)}>
+                {props.texte}
+            </div>
+
+            {Aberta && (
+                <div className={`p-5`}>
+                    {props.resposta}
+                </div>
+            )}
+
         </div>
     )
 }
