62b48be07eb90282ca4b1e428ebb92971a0b33d6
Carloseduardomelo <127193594+Carloseduardomelo@users.noreply.github.com>
2023-07-12 15:14:04 -0300

    pags de condicionais

---
+++ Curso-de-react-e-nextjs/app-inicial/src/components/condicional/Per.tsx
@@ -0,0 +1,7 @@
+export default function Pergunta(){
+    return(
+        <div>
+            pergunta
+        </div>
+    )
+}
---
+++ Curso-de-react-e-nextjs/app-inicial/src/pages/condicionalpags/faqpags.tsx
@@ -0,0 +1,9 @@
+import Faq from "@/components/condicional/Faq";
+
+export default function faqpags(){
+    return(
+        <div>
+            <Faq />
+        </div>
+    )
+}
