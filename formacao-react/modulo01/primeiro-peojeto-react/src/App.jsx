49a8074462869cdee2c21e8fd38c7223a6753a2f
carlos <127193594+Carloseduardomelo@users.noreply.github.com>
2023-08-15 22:29:29 -0300

    primeiro projeto em react

---
+++ formacao-react/modulo01/primeiro-peojeto-react/src/App.jsx
@@ -0,0 +1,14 @@
+//import { useState } from 'react'
+
+import But from "./botao/intex"
+
+function App() {
+  return (
+    <div>
+      <p>carlos</p>
+      <But cont="nada"/>
+    </div>
+  )
+}
+
+export default App
---
+++ formacao-react/modulo01/primeiro-peojeto-react/src/botao/intex.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+export default function But(props) {
+
+    const {cont} = props
+
+    return (
+    <div>
+        <button type="buttom">{cont}</button>
+    </div>
+    )
+}
